3f35cbe0f6769a7628fa7f7824c0e20beaf16847
skeres <stephane.keresztes@gmail.com>
2021-09-20 16:10:01 +0200

    initial

---
+++ src/test/java/com/sks/discount01/Discount01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sks.discount01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Discount01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
